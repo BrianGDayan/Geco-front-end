50657dc886eeb505efa86bf5d3c53b390f2f2996
Brian <42729519@fi.unju.edu.ar>
2025-04-17 11:59:01 -0300

    .

---
+++ src/app/(admin)/planillas-actuales/page.tsx
@@ -0,0 +1,8 @@
+export default function PlanillasActuales() {
+    return (
+      <div>
+        <h1>Listado de Planillas Actuales</h1>
+        <p>Aquí se mostrará el listado de planillas actuales.</p>
+      </div>
+    );
+  }
