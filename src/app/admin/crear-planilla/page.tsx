13df58d0f8cd4d4bb03d8cc32d24c4127970d202
Brian <42729519@fi.unju.edu.ar>
2025-05-23 12:08:00 -0300

    se modificaron campos del formulario

--- src/app/admin/crear-planilla/page.tsx
+++ src/app/admin/crear-planilla/page.tsx
@@ -12,31 +12,31 @@ export default function FormularioPlanilla() {
             <input type="text" id="obra" name="obra" className="border border-gray-border rounded p-2 mt-1" maxLength={30} required />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="nroPlano" className="text-lg">Obra</label>
+            <label htmlFor="nroPlano" className="text-lg">Número de plano</label>
             <input type="text" id="nroPlano" name="nroPlano" className="border border-gray-border rounded p-2 mt-1" maxLength={30} required />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="sector" className="text-lg">Obra</label>
+            <label htmlFor="sector" className="text-lg">Sector</label>
             <input type="text" id="sector" name="sector" className="border border-gray-border rounded p-2 mt-1" maxLength={30} required />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="encargadoElaborar" className="text-lg">Obra</label>
+            <label htmlFor="encargadoElaborar" className="text-lg">Encargado de elaborar</label>
             <input type="text" id="encargadoElaborar" name="encargadoElaborar" className="border border-gray-border rounded p-2 mt-1" maxLength={15} required />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="encargadoRevisar" className="text-lg">Obra</label>
+            <label htmlFor="encargadoRevisar" className="text-lg">Encargado de revisar</label>
             <input type="text" id="encargadoRevisar" name="encargadoRevisar" className="border border-gray-border rounded p-2 mt-1" maxLength={15} required />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="encargadoAprobar" className="text-lg">Obra</label>
+            <label htmlFor="encargadoAprobar" className="text-lg">Encargado de aprobar</label>
             <input type="text" id="encargadoAprobar" name="encargadoAprobar" className="border border-gray-border rounded p-2 mt-1" maxLength={15} />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="fecha" className="text-lg">Obra</label>
+            <label htmlFor="fecha" className="text-lg">Fecha</label>
             <input type="date" id="fecha" name="fecha" className="border border-gray-border rounded p-2 mt-1" required />
           </div>
           <div className="flex flex-col mt-4">
-            <label htmlFor="item" className="text-lg">Obra</label>
+            <label htmlFor="item" className="text-lg">Item</label>
             <input type="text" id="item" name="item" className="border border-gray-border rounded p-2 mt-1" maxLength={20} required />
           </div>
           <button className="flex justify-center mt-5 text-xl bg-primary hover:bg-primary-dark text-white p-3 rounded-md" >Ingresar</button>
