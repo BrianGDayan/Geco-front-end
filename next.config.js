680d47fbd3f9d14454ef28cd86bff81dfcf47470
brian <42729519@fi.unju.edu.ar>
2025-12-15 10:50:31 -0300

    -

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Ignora errores de linting en el build para ahorrar memoria
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // Ignora errores de tipo en el build para ahorrar memoria
     ignoreBuildErrors: true,
   },
 };
